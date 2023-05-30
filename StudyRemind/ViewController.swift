886afedacf852f7b3e7344fad08336c8838c0e4f
snake97123 <73522198+snake97123@users.noreply.github.com>
2023-05-30 15:40:22 +0900

    Initial Commit

---
+++ StudyRemind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyRemind
+//
+//  Created by 山下　航 on 2023/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
